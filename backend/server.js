479c4535d27508a59f29917ae7fafb4a6e1a79f1
drewstake <drewstake3@gmail.com>
2024-11-04 18:32:29 -0500

    Added screenshots

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-// backend/server.js
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -9,7 +8,7 @@ const requestRoutes = require('./routes/requests');
 const app = express();
 const PORT = 3000;
 
-app.use(cors());
+app.use(cors()); // Ensure this line is present
 app.use(bodyParser.json());
 
 app.use('/api/tenants', tenantRoutes);
